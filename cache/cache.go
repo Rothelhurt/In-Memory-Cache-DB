b57e620c68a7c86d82f1db46e6550529b0d1a6d1
d.kolesnik <d.kolesnik@samsung.com>
2024-11-08 13:16:08 +0300

    Add TTL to cache db

--- cache/cache.go
+++ cache/cache.go
@@ -1,25 +1,39 @@
 package cache
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
 type Cache struct {
-	db map[string]interface{}
+	db map[string]context.Context
 }
 
 func New() Cache {
-	db := make(map[string]interface{})
+	db := make(map[string]context.Context)
 	return Cache{
 		db: db,
 	}
 }
 
-func (c *Cache) Set(key string, value interface{}) {
-	c.db[key] = value
+func (c *Cache) Set(key string, value interface{}, ttl int) {
+
+	c.db[key], _ = context.WithTimeout(context.Background(), time.Duration(ttl)*time.Second)
+	c.db[key] = context.WithValue(c.db[key], key, value)
 }
 
 func (c Cache) Get(key string) interface{} {
 	if c.db[key] == nil {
 		return "There is no such key in DB."
 	}
-	return c.db[key]
+	select {
+	case <-c.db[key].Done():
+		delete(c.db, key)
+		return "Key was removed by timeout."
+	default:
+		return c.db[key].Value(key)
+	}
 }
 
 func (c Cache) Delete(key string) interface{} {
@@ -29,4 +43,10 @@ func (c Cache) Delete(key string) interface{} {
 	}
 	return "There is no such key in DB."
 
+}
+
+func (c Cache) ShowDB() {
+	for key := range c.db {
+		fmt.Printf("Key: %s, Value: %d\n", key, c.db[key].Value(key))
+	}
 }
--- main.go
+++ main.go
@@ -3,21 +3,37 @@ package main
 import (
 	"fmt"
 	"golang-ninja/hw1/cache"
+	"time"
 )
 
 func main() {
 	cache := cache.New()
 
-	cache.Set("userId", 42)
-	fmt.Println(cache)
+	cache.Set("userId", 42, 5)
 	userId := cache.Get("userId")
 
 	fmt.Println(userId)
-	nothing := cache.Get("Something")
-	fmt.Println(nothing)
 
-	fmt.Println(cache.Delete("userId"))
-	fmt.Println(cache.Delete("Something"))
+	time.Sleep(time.Second * 2)
+	UserId2 := cache.Get("userId")
+	fmt.Println(UserId2)
 
-	fmt.Println(cache.Get("userId"))
+	time.Sleep(time.Second * 6)
+	UserId3 := cache.Get("userId")
+	fmt.Println(UserId3)
+
+	cache.ShowDB()
+
+	// userId, err := cache.Get("userId")
+	// fmt.Println(cache)
+	// userId := cache.Get("userId")
+
+	// fmt.Println(userId)
+	// nothing := cache.Get("Something")
+	// fmt.Println(nothing)
+
+	// fmt.Println(cache.Delete("userId"))
+	// fmt.Println(cache.Delete("Something"))
+
+	// fmt.Println(cache.Get("userId"))
 }
